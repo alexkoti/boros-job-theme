7576589cc27b43bc20fd1d7da3ed7c4e7e7e84fa
Alex Koti <alexkoti@gmail.com>
2014-09-09 03:55:33 -0300

    limpar arquivo

--- functions/login.php
+++ functions/login.php
@@ -7,31 +7,20 @@
  * 
  */
 
-
-
-/* ========================================================================== */
-/* ADD ACTIONS/FILTERS ====================================================== */
-/* ========================================================================== */
-add_action( 'login_head', 			'custom_login_head' );
-add_filter( 'login_headerurl', 		'custom_login_headerurl' );
-add_filter( 'login_headertitle', 	'custom_login_headertitle' );
-
-
-
-/* ========================================================================== */
-/* ADICIONAR LOGIN PERSONALIZADO ============================================ */
-/* ========================================================================== */
 // adicionar css
+add_action( 'login_head', 'custom_login_head' );
 function custom_login_head(){ 
 	echo '<link rel="stylesheet" type="text/css" href="' . THEME . '/css/login.css" />'; 
 }
 
 // link do logo
+add_filter( 'login_headerurl', 'custom_login_headerurl' );
 function custom_login_headerurl(){
 	return home_url();
 }
 
 // atributo title no logo -> o texto sempre será o nome do site
+add_filter( 'login_headertitle', 'custom_login_headertitle' );
 function custom_login_headertitle(){
 	return get_bloginfo('name');
 }
