f15b9ca7dd346e7569219d7acdc23e813ac36b56
Alex Koti <alexkoti@gmail.com>
2014-09-09 02:11:28 -0300

    head.php apenas quando não for admin

--- functions.php
+++ functions.php
@@ -20,9 +20,9 @@ include_once('functions/login.php');					// login personalizado
 include_once('functions/general_config.php');			// configuração geral, que afeta admin e frontend
 include_once('functions/media.php');					// funções de midia, imagens, galerias
 include_once('functions/menus_sidebars_widgets.php');	// configuração de menus e sidebars e widgets
-include_once('functions/head.php');						// configurações para o <head> (scripts, css, etc) para frontend e admin
 if( !is_admin() ){
 	include_once('mini_templates.php');					// pequenos pedaços de templates reutilizáveis - são chamados como funções, porém focadas apenas no output, por isso deixado junto com os arquivos de templates comuns
+	include_once('functions/head.php');					// configurações para o <head> (scripts, css, etc) para frontend e admin
 	include_once('functions/frontend.php');				// funções específicas para o frontend site em questão e provavelmente não serão integradas ao framework
 	include_once('functions/template_redirects.php');	// redirecionamentos para templates adicionais
 }
