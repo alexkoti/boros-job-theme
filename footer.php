900cfea433cf85e8525618ba31e1e4ab8fb46ec8
Alex Koti <alexkoti@gmail.com>
2016-01-26 02:11:40 -0200

    Separação do <head> em template próprio e melhoria no locked.php

--- footer.php
+++ footer.php
@@ -1,16 +1,18 @@
 
 <footer id="footer">
     <div class="container">
-        <div class="col-md-12">
-            <?php
-            $args = array(
-                'theme_location'  => 'menu_footer', 
-                'container'       => false,
-                'container_class' => 'menu-footer',
-                'menu_class'      => 'nav nav-pills',
-            );
-            wp_nav_menu($args);
-            ?>
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                $args = array(
+                    'theme_location'  => 'menu_footer', 
+                    'container'       => false,
+                    'container_class' => 'menu-footer',
+                    'menu_class'      => 'nav nav-pills',
+                );
+                wp_nav_menu($args);
+                ?>
+            </div>
         </div>
     </div>
 </footer>
