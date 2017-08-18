351a0b3f8fe666d6eb8212e570ecaab504db2773
Alex Koti <alexkoti@gmail.com>
2017-08-17 22:41:54 -0300

    Campo textarea por último + comentários

--- comments.php
+++ comments.php
@@ -7,55 +7,55 @@
  * @link http://wpengineer.com/2214/adding-input-fields-to-the-comment-form/
  */
 ?>
-		<div id="comments">
-			
-		<?php
-		if( post_password_required() ){
-			echo '<p class="nopassword">Este post está protegido por senha.</p></div>';
-			return;
-		}
-		?>
+<div id="comments">
+    
+    <?php
+    if( post_password_required() ){
+        echo '<p class="nopassword">Este post está protegido por senha.</p></div>';
+        return;
+    }
+    ?>
 
-		<?php if( have_comments() ){ ?>
-			<h2>Comentários</h2>
-			
-			<?php
-			// comments navigations above
-			if( get_comment_pages_count() > 1 && get_option( 'page_comments' ) )
-				$paged_comments = true;
-			if( isset($paged_comments) && $paged_comments ){
-			?>
-			<div class="navigation">
-				<div class="nav-previous"><?php previous_comments_link( 'Comentários Antigos' ); ?></div>
-				<div class="nav-next"><?php next_comments_link( 'Comentários Recentes' ); ?></div>
-			</div> <!-- .navigation -->
-			<?php } ?>
+    <?php if( have_comments() ){ ?>
+        <h2>Comentários</h2>
+        
+        <?php
+        // comments navigations above
+        if( get_comment_pages_count() > 1 && get_option( 'page_comments' ) )
+            $paged_comments = true;
+        if( isset($paged_comments) && $paged_comments ){
+        ?>
+        <div class="navigation">
+            <div class="nav-previous"><?php previous_comments_link( 'Comentários Antigos' ); ?></div>
+            <div class="nav-next"><?php next_comments_link( 'Comentários Recentes' ); ?></div>
+        </div> <!-- .navigation -->
+        <?php } ?>
 
-				<ol class="dl_list dl_list_comments">
-					<?php
-					// incluir modelo de comment single
-					include_once('comment.php');
-					wp_list_comments( array('callback' => 'boros_comment_template') );
-					?>
-				</ol>
+            <ol class="dl_list dl_list_comments">
+                <?php
+                // incluir modelo de comment single
+                include_once('comment.php');
+                wp_list_comments( array('callback' => 'boros_comment_template') );
+                ?>
+            </ol>
 
-			<?php
-			// comments navigations below
-			if( isset($paged_comments) && $paged_comments ){
-			?>
-			<div class="navigation">
-				<div class="nav-previous"><?php previous_comments_link( 'Comentários Antigos' ); ?></div>
-				<div class="nav-next"><?php next_comments_link( 'Comentários Recentes' ); ?></div>
-			</div><!-- .navigation -->
-			<?php } ?>
+        <?php
+        // comments navigations below
+        if( isset($paged_comments) && $paged_comments ){
+        ?>
+        <div class="navigation">
+            <div class="nav-previous"><?php previous_comments_link( 'Comentários Antigos' ); ?></div>
+            <div class="nav-next"><?php next_comments_link( 'Comentários Recentes' ); ?></div>
+        </div><!-- .navigation -->
+        <?php } ?>
 
-		<?php } else { ?>
-		
-			<?php if( ! comments_open() ){ ?>
-			<p class="nocomments">Comentários fechados para este post.</p>
-			<?php } ?>
-			
-		<?php } ?>
+    <?php } else { ?>
+
+        <?php if( ! comments_open() ){ ?>
+        <p class="nocomments">Comentários fechados para este post.</p>
+        <?php } ?>
+        
+    <?php } ?>
 
 <?php
 /**
@@ -84,6 +84,22 @@ function bootstrap3_comment_form_fields( $fields ) {
     return $fields;
 }
 
+/**
+ * Campo de comentário por último
+ * 
+ */
+add_filter( 'comment_form_fields', 'wpb_move_comment_field_to_bottom' );
+function wpb_move_comment_field_to_bottom( $fields ) {
+    $comment_field = $fields['comment'];
+    unset( $fields['comment'] );
+    $fields['comment'] = $comment_field;
+    return $fields;
+}
+
+/**
+ * HTML para o campo textatrea de acordo com o bootstrap
+ * 
+ */
 add_filter( 'comment_form_defaults', 'bootstrap3_comment_form' );
 function bootstrap3_comment_form( $args ) {
     $args['comment_field'] = '<div class="form-group comment-form-comment">
@@ -98,4 +114,4 @@ function bootstrap3_comment_form( $args ) {
 comment_form();
 ?>
 
-		</div>
+</div>
